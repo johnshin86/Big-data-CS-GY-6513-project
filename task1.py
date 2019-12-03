080111fa314cdd84a21f29bafd423346f13409fe
Prathamesh D <pratham.d192@gmail.com>
2019-12-02 20:29:29 -0500

    Completed task 1 code

--- task1.py
+++ task1.py
@@ -1,9 +1,13 @@
 import os
+import csv
+import json
 import pyspark
 from pyspark import SparkContext
 
+import datetime
 from dateutil import parser
 
+from pyspark.ml.fpm import FPGrowth
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType
 from pyspark.sql.types import DoubleType
@@ -54,7 +58,7 @@ def castAsType(x):
 			return x
 
 def getIntStats(rdd):
-	subset = rdd.filter(lambda x:x[1] == "INTEGER").map(lambda x:x[0])
+	subset = rdd.filter(lambda x:x[1] == "INTEGER").map(lambda x: int(x[0]))
 	if subset.isEmpty():
 		return 0, 0, 0, 0, 0
 	else:
@@ -62,13 +66,13 @@ def getIntStats(rdd):
 		return count, subset.max(), subset.min(), subset.mean(), subset.stdev()
 
 def getRealStats(rdd):
-	subset = rdd.filter(lambda x:x[1] == "REAL").map(lambda x:x[0])
+	subset = rdd.filter(lambda x:x[1] == "REAL").map(lambda x:float(x[0]))
 	if subset.isEmpty():	return 0, 0, 0, 0, 0
 	count = subset.countApprox(timeout=10000)
 	return count, subset.max(), subset.min(), subset.mean(), subset.stdev()
 
 def getDateStats(rdd):
-	subset = rdd.filter(lambda x: x[1]=="DATE/TIME").map(lambda x: parse(x[0]))
+	subset = rdd.filter(lambda x: x[1]=="DATE/TIME").map(lambda x: parser.parse(x[0]))
 	if subset.isEmpty(): return 0, '', ''
 	count = subset.countApprox(timeout=10000)
 	return count, subset.max(), subset.min()
@@ -82,45 +86,58 @@ def getTextStats(rdd):
 	avgLen = subset.map(lambda x: x[1]).mean()
 	return count, longest, shortest, avgLen
 
+def datetime_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    raise TypeError("Unknown type")
 
 files = getFileList()
 
+freqColItems = []
+freqId = 0
 #Processing should be done inside for loop for each dataset
 for file in files:
 	
+	data = {}
+	data["dataset_name"] = file
 	df = getData(file).cache()
 	rdd = df.rdd.cache()
-	
-
 	# 1 & 2
 	emptyDf = df.select([count(when(isnan(c) | col(c).contains('NA') | col(c).contains('NULL') | col(c).isNull(),c)).alias(c) for c in df.columns])
+	emptyCount = emptyDf.rdd.map(lambda row : row.asDict()).collect()[0]
 	rows = rdd.countApprox(timeout=10000)
-	emptyCount = emptyDf.collect()[0]
 	nonEmptyCount = {}
 	for c in emptyDf.columns:
 		nonEmptyCount[c] = rows - emptyCount[c]
-
-
+	
 	#3 & 4 & 5
 	mostFrequent = {}
 	distinct = {}
 	dataTypes = dict(df.dtypes)
 	colStats = {}
+	colListData = []
+	
 	for col in df.columns:
+		#print(col)
+		colData = {}
+		colData["column_name"] = col
+		colData["number_non_empty_cells"] = nonEmptyCount[col]
+		colData["number_empty_cells"] = emptyCount[col]
 		grouped = df.groupBy(col).count()
 		distinctCount = grouped.count()
+		colData["number_distinct_values"] = distinctCount
 		tempFreq = grouped.sort(F.desc('count')).select(col).take(5)
 		freqList = []
 		for item in tempFreq:
 			freqList.append(item[0])
-
+		colData["frequent_values"] = freqList
 		mostFrequent[col] = freqList
 		distinct[col] = distinctCount
-	
-		#Dont need count, already calculated above
 		if dataTypes[col] == "string":
 			colRdd = df.select(col).dropna().rdd.map(lambda x: x[0])
 			colRdd = colRdd.map(lambda x: (x,castAsType(x)))
+			freqColItems.append((freqId,colRdd.map(lambda x:x[1]).distinct().collect()))
+			freqId += 1
 			intStats = getIntStats(colRdd)
 			realStats = getRealStats(colRdd)
 			dateStats = getDateStats(colRdd)
@@ -128,23 +145,79 @@ for file in files:
 			colStats[col] = [intStats,realStats,dateStats,textStats]
 		elif dataTypes[col] in ["int","long","bigint"]:
 			colRdd = df.select(col).dropna().rdd.map(lambda x: x[0])
-			count = colRdd.countApprox(timeout=10000)
-			intStats = (count, colRdd.max(), colRdd.min(), colRdd.mean(), colRdd.stdev())
+			intStats = (nonEmptyCount[col], colRdd.max(), colRdd.min(), colRdd.mean(), colRdd.stdev())
 			realStats = (0,0,0,0,0)
 			dateStats = (0,0,0)
 			textStats = (0,0,0,0)
 			colStats[col] = [intStats,realStats,dateStats,textStats]
 		elif dataTypes[col] in ["double","float"]:
 			colRdd = df.select(col).dropna().rdd.map(lambda x: x[0])
-			count = colRdd.countApprox(timeout=10000)
 			intStats = (0,0,0,0,0)
-			realStats = (count, colRdd.max(), colRdd.min(), colRdd.mean(), colRdd.stdev())
+			realStats = (nonEmptyCount[col], colRdd.max(), colRdd.min(), colRdd.mean(), colRdd.stdev())
 			dateStats = (0,0,0)
 			textStats = (0,0,0,0)
 			colStats[col] = [intStats,realStats,dateStats,textStats]
-
-
 		colRdd.unpersist()
+		
+		dataTypesValue = {}
+		
+		for key in colStats.keys():
+			keyData = colStats[key]
+			#0 Int,1 real, 2 date, 3 text
+			tempArray = []
+			if keyData[0][0] != 0:
+				tempData = {}
+				tempData["type"] = "INTEGER (LONG)"
+				tempData["count"] = keyData[0][0]
+				tempData["max_value"] = keyData[0][1]
+				tempData["min_value"] = keyData[0][2]
+				tempData["mean"] = keyData[0][3]
+				tempData["stddev"] = keyData[0][4]
+				tempArray.append(tempData)
+			if keyData[1][0] != 0:
+				tempData = {}
+				tempData["type"] = "REAL"
+				tempData["count"] = keyData[1][0]
+				tempData["max_value"] = keyData[1][1]
+				tempData["min_value"] = keyData[1][2]
+				tempData["mean"] = keyData[1][3]
+				tempData["stddev"] = keyData[1][4]
+				tempArray.append(tempData)
+			if keyData[2][0] != 0:
+				tempData = {}
+				tempData["type"] = "DATE/TIME"
+				tempData["count"] = keyData[2][0]
+				tempData["max_value"] = keyData[2][1]
+				tempData["min_value"] = keyData[2][2]
+				tempArray.append(tempData)
+			if keyData[3][0] != 0:
+				tempData = {}
+				tempData["type"] = "TEXT"
+				tempData["count"] = keyData[3][0]
+				tempData["shortest_values"] = keyData[3][2]
+				tempData["longest_values"] = keyData[3][1]
+				tempData["average_length"] = keyData[3][3]
+				tempArray.append(tempData)
+			dataTypesValue[key] = tempArray
+		colData["dataTypes"] = dataTypesValue[col]
+		colListData.append(colData)
+	
+	data["columns"] = colListData
+
+	with open(file.split("/")[1] +'.json', 'w') as fp:
+    	json.dump(data, fp,default=datetime_handler)
 
 	df.unpersist()
 	rdd.unpersist()
+
+freqDf = spark.createDataFrame(freqColItems,["id","dtypes"]
+fpGrowth = FPGrowth(itemsCol="dtypes", minSupport=0.5, minConfidence=0.6)
+model = fpGrowth.fit(df)
+freqSet = model.freqItemsets.collect()
+
+with open('freqDataTypes.csv','w') as f:
+    wr = csv.writer(f)
+    for item in freqSet:
+    	if len(item[0]) > 1:
+    		wr.writerow(item[0])
+
