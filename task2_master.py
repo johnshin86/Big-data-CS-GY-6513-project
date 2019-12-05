dfb6200ae5e6b0296b35d20398e882e325bcfbf8
John Shin <johnyoungshin@gmail.com>
2019-12-05 17:21:21 -0500

    task2_master progress

--- task2_master.py
+++ task2_master.py
@@ -40,35 +40,36 @@ with open("/home/jys308/cluster1.txt","r") as f:
 
 files = content[0].strip("[]").replace("'","").replace(" ","").split(",")
 
-def semanticType(df):
+def semanticType(colName, df):
     types = {}
     """
     The semantic types:
-    1) Person name
-    2) Business name
-    3) Phone Number
-    4) Address
-    5) Street name
-    6) City
-    7) Neighborhood
-    8) LAT/LON coordinates
-    9) Zip code
-    10) Borough
-    11) School name (Abbreviations and full names)
-    12) Color
-    13) Car make
-    14) City agency (Abbreviations and full names)
-    15) Areas of study (e.g., Architecture, Animal Science, Communications)
-    16) Subjects in school (e.g., MATH A, MATH B, US HISTORY)
-    17) School Levels (K-2, ELEMENTARY, ELEMENTARY SCHOOL, MIDDLE)
-    18) College/University names
-    19) Websites (e.g., ASESCHOLARS.ORG)
-    20) Building Classification (e.g., R0-CONDOMINIUM, R2-WALK-UP)
-    21) Vehicle Type (e.g., AMBULANCE, VAN, TAXI, BUS)
-    22) Type of location (e.g., ABANDONED BUILDING, AIRPORT TERMINAL, BANK, CHURCH, CLOTHING/BOUTIQUE)
-    23) Parks/Playgrounds (e.g., CLOVE LAKES PARK, GREENE PLAYGROUND)
+    1) Person name (Last name, First name, Middle name, Full name) NAME
+    2) Business name NAME
+    3) Phone Number REGEX
+    4) Address REGEX
+    5) Street name NAME
+    6) City LEVEN
+    7) Neighborhood LEVEN
+    8) LAT/LON coordinates REGEX
+    9) Zip code REGEX
+    10) Borough LEVEN
+    11) School name (Abbreviations and full names) LEVEN
+    12) Color LEVEN
+    13) Car make LEVEN
+    14) City agency (Abbreviations and full names) LEVEN
+    15) Areas of study (e.g., Architecture, Animal Science, Communications) LEVEN
+    16) Subjects in school (e.g., MATH A, MATH B, US HISTORY) LEVEN
+    17) School Levels (K-2, ELEMENTARY, ELEMENTARY SCHOOL, MIDDLE) LEVEN
+    18) College/University names LEVEN
+    19) Websites (e.g., ASESCHOLARS.ORG) REGEX
+    20) Building Classification (e.g., R0-CONDOMINIUM, R2-WALK-UP) LEVEN
+    21) Vehicle Type (e.g., AMBULANCE, VAN, TAXI, BUS) LEVEN
+    22) Type of location (e.g., ABANDONED BUILDING, AIRPORT TERMINAL, BANK, CHURCH, CLOTHING/BOUTIQUE) LEVEN
+    23) Parks/Playgrounds (e.g., CLOVE LAKES PARK, GREENE PLAYGROUND) LEVEN
 
     Dataframe will be 2 columns. First column is the data and 2nd is the count.
+    We will check the column name and it's levenshtein distance with the list of semantic types. We will call the    function for that semantic type.
     """
 
 
@@ -87,5 +88,7 @@ for file in files:
 	fileName = fileData[0]
 	colName = fileData[1]
 	df = spark.read.option("delimiter", "\\t").option("header","true").option("inferSchema","true").csv("/user/hm74/NYCColumns/" + file)
+        out = semanticTypes(colName, df)
+        #process dictionary to record to json
 
 
