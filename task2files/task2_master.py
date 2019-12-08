170ddfc17a09edbedda371d1b9e78127d542f5c0
johnshin86 <johnyoungshin@gmail.com>
2019-12-08 17:14:23 -0500

    fixing typo

--- task2files/task2_master.py
+++ task2files/task2_master.py
@@ -667,7 +667,7 @@ for file in files[:10]:
     print("This is column number", files.index(file))
     #process dictionary to record to json
     with open(str(colName) +'.json', 'w') as fp:
-        json.dump(types, fp,default=datetime_handler)
+        json.dump(types, fp)
 
 """
 
